99b6abfc76a1d90e6959eb5bbe14a04c287b3f8a
angus-galbraith <angus.galbraith25@outlook.com>
2021-06-25 06:08:58 +0100

    starting to add frames

--- dartstk.py
+++ dartstk.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.constants import BOTH, GROOVE, VERTICAL, Y
 
 
 
@@ -7,3 +8,37 @@ class MainScreen(tk.Tk):
         super().__init__()
         self.title("Darts")
         self.geometry('800x600')
+
+        # create menu items 
+        self.menu = tk.Menu(self, bg="lightgrey", fg="black")
+        self.game_menu = tk.Menu(self.menu, tearoff=0, bg="lightgrey", fg="black")
+        self.game_menu.add_command(label="New Game", command=self.new_game)
+
+        self.menu.add_cascade(label="Game Options", menu=self.game_menu)
+        self.config(menu=self.menu)
+
+        
+
+        # create frames
+        name_frame = tk.Frame(self, background='blue', relief=GROOVE)
+        name_frame.pack(side=tk.TOP, fill=BOTH)
+        
+        pl1stats_frame = tk.Frame(self, background='red')
+        pl1stats_frame.pack(side=tk.LEFT, fill=BOTH)
+        pl1sets_label = tk.Label(pl1stats_frame, text='Sets')
+        pl1sets_label.pack()
+
+        score_frame = tk.Frame(self, background='yellow')
+        score_frame.pack(side=tk.LEFT, fill=BOTH)
+        score_label = tk.Label(score_frame, text='remaining')
+        score_label.pack()
+
+        #pl2stats_frame = tk.Frame(self, background='red')
+        #pl2stats_frame.pack(side=tk.LEFT, fill=BOTH)
+
+    def new_game(self):
+        pass
+
+if __name__ == "__main__":
+    screen = MainScreen()
+    screen.mainloop()
