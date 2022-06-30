02586bb5bdb4364d9ddc89fba69e5acaf97dfc43
angus-galbraith <angus.galbraith25@outlook.com>
2022-06-30 21:48:42 +0100

    menu items added

--- roundtheboard22.1.py
+++ roundtheboard22.1.py
@@ -19,18 +19,29 @@ class RoundTheBoard(tk.Tk):
         self.title("Round the Board")
         #self.geometry('900x600')
 
-
         self.menu = tk.Menu(self, bg="lightgrey", fg="black")
+        # first menu item
         self.game_menu = tk.Menu(self.menu, tearoff=0, bg="lightgrey", fg="black")
         self.game_menu.add_command(label="New Game", command=self.new_game)
-        #self.pack_propagate(0)
         self.menu.add_cascade(label="Game Options", menu=self.game_menu)
+
+        # second menu item
+        self.stat_menu = tk.Menu(self.menu, tearoff=0, bg="lightgrey", fg="black")
+        self.stat_menu.add_command(label="Stats", command=self.stats_window)
+        self.menu.add_cascade(label="Statistics", menu=self.stat_menu)
+
+
         self.config(menu=self.menu)
+
 
     def new_game(self):
         pass
 
-    
+
+    def stats_window(self):
+        pass
+
+
 if __name__ == "__main__":
     game = RoundTheBoard()
     game.mainloop()
